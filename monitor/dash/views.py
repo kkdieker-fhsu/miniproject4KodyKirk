e699bc26aa57e41e6f3634c8f40158c04f8d2764
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-11-08 12:54:31 -0900

    remove unused import from views.py

--- monitor/dash/views.py
+++ monitor/dash/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Endpoints, TrafficLog
 from .forms import registerendpoint
 
