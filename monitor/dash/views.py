f70b3f847be6cae433025ed4b0e04332847991fb
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-11-08 08:13:42 -0900

    add new templates

--- monitor/dash/views.py
+++ monitor/dash/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Endpoints, TrafficLog
 
 
@@ -9,12 +9,14 @@ def index(request):
     return render(request, "dash/index.html", output)
 
 def endpoints(request):
-    return HttpResponse("Hello, world - endpoints.")
+    endpoint_list = Endpoints.objects.order_by('ip_address')
+    output = {'endpoint_list': endpoint_list}
+    return render(request, "dash/endpoints.html", output)
 
 def traffic(request):
     return HttpResponse("Hello, world - traffic.")
 
 def detail(request, ip_address):
     endpoint = get_object_or_404(Endpoints, pk=ip_address)
-    traffic = TrafficLog.objects.filter(ip_src=endpoint.ip_address).order_by('-interval_start')
+    traffic = get_list_or_404(TrafficLog, ip_src=endpoint)
     return render(request, "dash/detail.html", {'endpoint': endpoint, 'traffic': traffic})
