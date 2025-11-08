e95a1ea923c257ccbc1acf9d2fb6dec986b5e997
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-11-08 11:55:37 -0900

    starting to work on forms with basic experimental form

--- monitor/dash/views.py
+++ monitor/dash/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Endpoints, TrafficLog
+from .forms import NameForm
 
 
 def index(request):
@@ -27,5 +28,23 @@ def endpoint_register(request):
     return render(request, "dash/endpoint_register.html")
 
 def endpoint_submission(request):
+
     return HttpResponseRedirect(reverse("dash:endpoints"))
 
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, "endpoints.html", {"form": form})
