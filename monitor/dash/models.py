dac4b2fd811ce5e92b1d87d21e2afdfa325b64b7
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-11-08 06:01:26 -0900

    issues

--- monitor/dash/models.py
+++ monitor/dash/models.py
@@ -7,8 +7,8 @@ class Endpoints(models.Model):
     hostname = models.CharField(max_length=255)
     last_seen = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-    def __str__(self):
-        return self.ip_address
+    #def __str__(self):
+    #    return self.ip_address
 
 class TrafficLog(models.Model):
     pk = models.CompositePrimaryKey('ip_src', 'ip_dst')
--- monitor/dash/views.py
+++ monitor/dash/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 
+from monitor.dash.models import Endpoints
+
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    endpoint_list = Endpoints.objects.order_by('ip_address')
+    output = ", ".joing([e.ip_address for e in endpoint_list])
+    return HttpResponse(output)
+
+
